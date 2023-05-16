062b01b1b57baa0c41fb9fda1362879c815038e7
Felix <felix.zhao2@student.tdsb.on.ca>
2023-05-16 14:04:50 -0400

    sound kinda works

--- Main.java
+++ Main.java
@@ -2,8 +2,9 @@
 /// THIS IS A COMMENT
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello Poopoo 2 3!");
+        Sound test = new Sound("C:/Users/Felix/Documents/Timber Trek/2023-ICS4-Final-Project/boom.wav");
+        test.play();
     }
 }
---
+++ Sound.java
@@ -0,0 +1,45 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Sound {
+    private File soundFile;
+    private AudioInputStream audioStream;
+    private AudioFormat audioFormat;
+    private DataLine.Info info;
+    private SourceDataLine sourceDataLine;
+    private static final int BUFFER_SIZE = 4096;
+
+    public Sound(String filePath) {
+        try {
+            soundFile = new File(filePath);
+            audioStream = AudioSystem.getAudioInputStream(soundFile);
+            audioFormat = audioStream.getFormat();
+            info = new DataLine.Info(SourceDataLine.class, audioFormat);
+            sourceDataLine = (SourceDataLine) AudioSystem.getLine(info);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        try {
+            sourceDataLine.open(audioFormat);
+            sourceDataLine.start();
+
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead;
+
+            while ((bytesRead = audioStream.read(buffer, 0, buffer.length)) != -1) {
+                sourceDataLine.write(buffer, 0, bytesRead);
+            }
+
+            sourceDataLine.drain();
+            sourceDataLine.stop();
+            sourceDataLine.close();
+            audioStream.close();
+        } catch (IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
