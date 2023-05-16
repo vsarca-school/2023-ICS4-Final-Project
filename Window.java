8d2b9195e8f58d81d9afb6b1d16bb017d22c0463
vsarca <vsarca@outlook.com>
2023-05-16 09:54:42 -0400

    Instructions

--- Window.java
+++ Window.java
@@ -8,8 +8,17 @@ import java.awt.*;
  * 
  * The JComponent class has a paint method - this does NOT update in real time,
  * only when you move the window. (I think). Create a test setup and make sure
- * you can RUN IT AT 60 FPS. Also, the drawing class will be outsourced to the
- * game, so this
+ * you can RUN IT AT 60 FPS. Also, the drawing will be outsourced to the game
+ * class.
+ * 
+ * WHAT DO WE WANT:
+ * We want to have a canvas that we can update at will, 60 times per second. We
+ * can draw to the canvas as much as we want, and it will update the screen when
+ * we tell it to, and will clear the screen when we want it to.
+ * WE DO NOT WANT A PAINT METHOD. We don't want to draw all the graphics when
+ * Java wants us to, we want to draw it when its convenient for us. That is how
+ * most renderers work, including the default one from the OS so it must be
+ * possible in Java.
  */
 public class Window {
     public Window() {
