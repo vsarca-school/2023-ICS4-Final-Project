ef76f3a6462aa66e140c3264178f96135d0b5aeb
Felix <felix.zhao2@student.tdsb.on.ca>
2023-05-17 12:42:41 -0400

    zounds

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,11 @@ import src.main.Window;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Poopoo 2 3!");
-        Sound test = new Sound("C:/Users/Felix/Documents/Timber Trek/2023-ICS4-Final-Project/boom.wav");
-        test.play();
+        Sound test = new Sound("2023-ICS4-Final-Project/src/main/Sounds/boom.wav");
+        Sound amogus = new Sound("2023-ICS4-Final-Project/src/main/Sounds/amongus.wav");
+        Sound duck = new Sound("2023-ICS4-Final-Project/src/main/Sounds/duck.wav");
+        duck.play();
+        amogus.loop(5);
         new Window();
     }
 }
--- src/main/Sound.java
+++ src/main/Sound.java
@@ -1,4 +1,5 @@
 package src.main;
+
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
@@ -9,7 +10,7 @@ public class Sound {
     private AudioFormat audioFormat;
     private DataLine.Info info;
     private SourceDataLine sourceDataLine;
-    private static final int BUFFER_SIZE = 4096;
+    private static final int BUFFER_SIZE = 12288;
 
     public Sound(String filePath) {
         try {
@@ -24,23 +25,59 @@ public class Sound {
     }
 
     public void play() {
-        try {
-            sourceDataLine.open(audioFormat);
-            sourceDataLine.start();
+        Thread soundThread = new Thread(() -> {
+            try {
+                sourceDataLine.open(audioFormat);
+                sourceDataLine.start();
 
-            byte[] buffer = new byte[BUFFER_SIZE];
-            int bytesRead;
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int bytesRead;
 
-            while ((bytesRead = audioStream.read(buffer, 0, buffer.length)) != -1) {
-                sourceDataLine.write(buffer, 0, bytesRead);
+                while ((bytesRead = audioStream.read(buffer, 0, buffer.length)) != -1) {
+                    sourceDataLine.write(buffer, 0, bytesRead);
+                }
+
+                sourceDataLine.drain();
+                sourceDataLine.stop();
+                sourceDataLine.close();
+                audioStream.close();
+            } catch (IOException | LineUnavailableException e) {
+                e.printStackTrace();
             }
-
-            sourceDataLine.drain();
-            sourceDataLine.stop();
-            sourceDataLine.close();
-            audioStream.close();
-        } catch (IOException | LineUnavailableException e) {
-            e.printStackTrace();
-        }
+        });
+        soundThread.start();
     }
+
+    public void loop(int times) {
+        Thread soundThread = new Thread(() -> {
+            try {
+                sourceDataLine.open(audioFormat);
+                sourceDataLine.start();
+    
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int bytesRead;
+    
+                for (int i = 0; i < times; i++) {
+                    audioStream = AudioSystem.getAudioInputStream(soundFile);
+    
+                    while ((bytesRead = audioStream.read(buffer, 0, buffer.length)) != -1) {
+                        sourceDataLine.write(buffer, 0, bytesRead);
+                    }
+    
+                    // Rewind the audio stream to the beginning for the next loop
+                    audioStream.reset();
+                }
+    
+                sourceDataLine.drain();
+                sourceDataLine.stop();
+                sourceDataLine.close();
+                audioStream.close();
+            } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
+                e.printStackTrace();
+            }
+        });
+    
+        soundThread.start();
+    }
+
 }
