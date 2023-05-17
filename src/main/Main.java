6d1cd2cc637499a24de2bfa5211a04ca780bdfb9
RadinAhari <radin.farajzadehahari@student.tdsb.on.ca>
2023-05-17 09:33:23 -0400

    JIAUHJG

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,8 @@ import src.main.Window;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Poopoo 2 3!");
-        Sound test = new Sound("C:/Users/Felix/Documents/Timber Trek/2023-ICS4-Final-Project/boom.wav");
-        test.play();
+        //Sound test = new Sound("C:/Users/Felix/Documents/Timber Trek/2023-ICS4-Final-Project/boom.wav");
+        //test.play();
         new Window();
     }
 }
