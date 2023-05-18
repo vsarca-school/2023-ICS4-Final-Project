57da7bb2040a75a4b60da025c0329e8c9f1044a3
vsarca <vsarca@outlook.com>
2023-05-18 09:33:06 -0400

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,9 +5,9 @@ import src.main.Window;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Poopoo 2 3!");
-        Sound test = new Sound("2023-ICS4-Final-Project/src/main/Sounds/boom.wav");
-        Sound amogus = new Sound("2023-ICS4-Final-Project/src/main/Sounds/amongus.wav");
-        Sound duck = new Sound("2023-ICS4-Final-Project/src/main/Sounds/duck.wav");
+        Sound test = new Sound("src/main/Sounds/boom.wav");
+        Sound amogus = new Sound("src/main/Sounds/amongus.wav");
+        Sound duck = new Sound("src/main/Sounds/duck.wav");
         duck.play();
         amogus.loop(5);
         new Window();
