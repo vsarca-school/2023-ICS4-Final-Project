5d14cd9f14e9b5b82d56f099ad06538c1683bec9
vsarca <vsarca@outlook.com>
2023-05-23 09:45:52 -0400

    .

--- src/main/Levels/LevelCreator.java
+++ src/main/Levels/LevelCreator.java
@@ -1,6 +1,16 @@
 package src.main.Levels;
+
 import src.main.Level;
 
+/**
+ * This class extends the Level class in order to access all of its protected
+ * fields and modify them. This class will be used to create level objects with
+ * specifc setups
+ *      - Victor
+ */
+
 public class LevelCreator extends Level {
-    
+    public static void main(String argv[]) {
+        ;
+    }
 }
