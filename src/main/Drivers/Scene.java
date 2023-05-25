64152c140d6b711e06674a28854bc9091790eef4
vsarca <vsarca@outlook.com>
2023-05-25 09:44:15 -0400

    .

---
+++ src/main/Drivers/Scene.java
@@ -0,0 +1,13 @@
+package src.main.Drivers;
+
+/**
+ * This is an interface implemented by all scenes
+ *      - Victor
+ */
+public interface Scene {
+    /**
+     * 
+     * @return
+     */
+    public char change();
+}
