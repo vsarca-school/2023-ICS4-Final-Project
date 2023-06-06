39e5eb401258d6f9a54181143c3332ebdf8b0d96
vsarca <vsarca@outlook.com>
2023-06-06 09:37:23 -0400

    .

--- src/main/Drivers/Player.java
+++ src/main/Drivers/Player.java
@@ -6,7 +6,8 @@ import java.awt.Graphics;
  * Hnadles all player movement, plus rendering
  */
 public class Player implements ScreenElement {
-    Level l;
+    private double scale;
+    private Level l;
 
     /**
      * - Victor
@@ -21,8 +22,15 @@ public class Player implements ScreenElement {
      * - Victor/Radin
      */
     public void update(Window w, Graphics g) {
-        g.drawImage(Sprite.getTile("player-0"), w.getWidth()/2, w.getHeight()/2, null);
-        g.drawRect(20, 20, 20, 20);
+        // Calculate scaling and centering
+        double hww = w.getWidth() / 2.0;
+        double hwh = w.getHeight() / 2.0;
+        scale = Math.sqrt(hww * hwh) / 5;
+        hww -= scale/2;
+        hwh -= scale/2;
+        // Render player
+        g.drawImage(Sprite.getTile("player-0").getScaledInstance((int)scale+1, (int)scale+1, Image.SCALE_DEFAULT), (int) hww,
+                        (int) hwh, null);
     }
 
     public void addToWindow(Window w) {
