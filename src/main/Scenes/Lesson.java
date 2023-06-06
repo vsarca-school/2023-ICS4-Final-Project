4dce8f048c588f2eeaf237daddc332a4ad80c4c6
Felix <felix.zhao2@student.tdsb.on.ca>
2023-06-06 09:24:54 -0400

    a

--- src/main/Scenes/Lesson.java
+++ src/main/Scenes/Lesson.java
@@ -36,17 +36,6 @@ public class Lesson implements Serializable, ScreenElement {
         return lesson;
     }
 
-    /*  Custom JPanel for drawing
-    class DrawingPanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            if (currentStringIndex < texts.length) {
-                centerString(g, texts[currentStringIndex].substring(0, currentIndex), 400, 250, new Font("Arial", Font.PLAIN, 16));
-            }
-        }
-    }*/
-
     public void centerString(Graphics g, String text, int x, int y, Font font) {
         g.setFont(font);
         FontMetrics metrics = g.getFontMetrics();
@@ -65,22 +54,6 @@ public class Lesson implements Serializable, ScreenElement {
     }
 
     public void update(Window w, Graphics g) {
-        // // Update slide
-        // if (slideIndex < positions.length && currentIndex == positions[slideIndex]) {
-        //     //draw image images[slideIndex];
-        //     slideIndex++;
-        // }
-        // currentIndex++;
-        // if (currentIndex > texts[currentStringIndex].length() + 10) {
-        //     currentIndex = 0; // Reset currentIndex
-        //     currentStringIndex++; // Move to the next string
-        // }
-
-        // // Draw lesson
-        // if (currentStringIndex < texts.length) {
-        //     centerString(g, texts[currentStringIndex].substring(0, Math.min(currentIndex, texts[currentStringIndex].length())), 400, 250, new Font("Arial", Font.PLAIN, 16));
-        // }
-        // timer++;
         if (timer % DELAY < DELAY) {
             centerString(g, texts[currentStringIndex].substring(0, Math.min(currentIndex, texts[currentStringIndex].length())), 400, 250, new Font("Arial", Font.PLAIN, 16));
         }
@@ -96,40 +69,4 @@ public class Lesson implements Serializable, ScreenElement {
             timer = 0;
         }
     }
-
-    /*/
-        //frame = w.getFrame();
-
-        // Create a custom DrawingPanel and add it to the frame
-        //DrawingPanel drawingPanel = new DrawingPanel();
-        //frame.add(drawingPanel);
-
-        //frame.setVisible(true);
-
-        // Start the typewriter animation on a separate thread
-        SwingWorker<Void, Void> animationWorker = new SwingWorker<Void, Void>() {
-            @Override
-            protected Void doInBackground() throws Exception {
-                while (currentStringIndex < texts.length) {
-                    //TODO add image implementation to the lessons
-                    if (slideIndex < positions.length && currentIndex == positions[slideIndex]) {
-                        //draw image images[slideIndex];
-                        slideIndex++;
-                    }
-                    if (currentIndex < texts[currentStringIndex].length()) {
-                        currentIndex++;
-                        SwingUtilities.invokeLater(() -> drawingPanel.repaint()); // Trigger a repaint of the panel on the EDT
-                        Thread.sleep(100); // Delay between characters
-                    } else {
-                        Thread.sleep(500); // Pause after each word
-                        currentIndex = 0; // Reset currentIndex
-                        currentStringIndex++; // Move to the next string
-                    }
-                }
-                return null;
-            }
-        };
-        animationWorker.execute();
-    }*/
-
 }
