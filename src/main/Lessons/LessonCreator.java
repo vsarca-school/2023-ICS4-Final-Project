6f74eb802765e87f31aca973956f12f8b119e738
Felix <felix.zhao2@student.tdsb.on.ca>
2023-06-06 10:20:39 -0400

    a

--- src/main/Lessons/LessonCreator.java
+++ src/main/Lessons/LessonCreator.java
@@ -18,9 +18,10 @@ public class LessonCreator {
                          "Moss also tends to grow on the northern side, but take these signs with a grain of salt as they may not always work.",
                          "When you have no other options left, remember to walk in a straight line, as people tend to walk in circles and get even more lost.",
                          "Focus on a landmark and walk towards it, you have a higher chance of rescue if you walk in a straight line rather than a circle."};
-        int[] pos = {-1};
+        int[] pos = {3, 5};
+        String[] images = {"raymond", "logo"};
         // Create level 1
-        Lesson l1 = new Lesson(strs, pos, null);
+        Lesson l1 = new Lesson(strs, pos, images);
         
         // Save level 1
         out = new ObjectOutputStream(new FileOutputStream("src/main/Lessons/Lesson-1.lsn"));
