93e35f5529f3456e34e20e98978557f7e308ca86
RadinAhari <radin.farajzadehahari@student.tdsb.on.ca>
2023-06-06 09:36:47 -0400

    e

--- src/main/Drivers/Player.java
+++ src/main/Drivers/Player.java
@@ -7,6 +7,10 @@ import java.awt.Graphics;
  */
 public class Player implements ScreenElement {
     Level l;
+    String[] walkUp = {"player-12","player-13","player-14","player-15"};
+    String[] walkDown = {"player-0","player-1","player-2","player-3"};
+    String[] walkRight = {"player-4","player-5","player-6","player-7"};
+    String[] walkLeft = {"player-8","player-9","player-10","player-11"};
 
     /**
      * - Victor
@@ -21,7 +25,9 @@ public class Player implements ScreenElement {
      * - Victor/Radin
      */
     public void update(Window w, Graphics g) {
-        g.drawImage(Sprite.getTile("player-0"), w.getWidth()/2, w.getHeight()/2, null);
+        for(int i = 0; i<4;i++){
+            g.drawImage(Sprite.getTile(walkUp[i]), w.getWidth()/2, w.getHeight()/2, null);
+        }
     }
 
     public void addToWindow(Window w) {
