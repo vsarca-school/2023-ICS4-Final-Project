bf6052f9eb5a60307f52f432c8bbdf4e1a20a348
vsarca <vsarca@outlook.com>
2023-06-08 10:08:04 -0400

    .

--- src/main/Scenes/Options.java
+++ src/main/Scenes/Options.java
@@ -10,19 +10,25 @@ import src.main.Drivers.Window;
 //////// TODOOOOOO
 //////// MAKE IT CLEANER REMOVE THE SCALE VAIRABLE
 
+// TODO: MAKE IT SEE THROUGH BY ADDING "PAUSE" METHOD TO SCREEN ELEMENTS
+
 public class Options implements ScreenElement {
     private int previousScene;
 
     public void update(Window w, Graphics g) {
         // Find scaling
+        // Find scaling
         double hww = w.getWidth() / 2.0;
         double hwh = w.getHeight() / 2.0;
-        double scale = Math.min(hww / 64, hwh / 48);
+        double scale = Sprite.getImageScale();
+        double screenFit = Sprite.getBgScale();
+        ;
 
         // Draw screen
+        //drawImage(g, Sprite.getScaledBackground("TitleScreen"), screenFit, (int) hww, (int) hwh);
         drawImage(g, Sprite.getScaledImage("paused"), scale, (int) (hww), (int) (hwh - 32 * scale));
         for (int i = 0; i < 5; i++) {
-            drawImage(g, Sprite.getScaledTile("vine-0"), scale, (int) (hww + (16 * i - 32) * scale),
+            drawImage(g, Sprite.getScaledImage("vine"), scale, (int) (hww + (16 * i - 32) * scale),
                     (int) (hwh - 16 * scale));
         }
         drawImage(g, Sprite.getScaledImage("continue"), scale, (int) (hww), (int) (hwh));
@@ -33,22 +39,25 @@ public class Options implements ScreenElement {
         while ((mouse = w.nextMouse()) != null) {
             if (isClicked(Sprite.getScaledImage("continue"), scale, (int) (hww), (int) (hwh), mouse)) {
                 Main.changeScene(previousScene);
-            } else if (isClicked(Sprite.getScaledImage("menuquit"), scale, (int) (hww), (int) (hwh + 16 * scale), mouse)) {
+            } else if (isClicked(Sprite.getScaledImage("menuquit"), scale, (int) (hww), (int) (hwh + 16 * scale),
+                    mouse)) {
                 Main.changeScene(0);
             }
         }
     }
 
     private void drawImage(Graphics g, Image image, double scale, int x, int y) {
-        /*int newWidth = (int) (scale * image.getWidth(null));
-        int newHeight = (int) (scale * image.getHeight(null));
-        g.drawImage(image.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH), x - newWidth / 2,
-                y - newHeight / 2, null);*/
-                
+        /*
+         * int newWidth = (int) (scale * image.getWidth(null));
+         * int newHeight = (int) (scale * image.getHeight(null));
+         * g.drawImage(image.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH),
+         * x - newWidth / 2,
+         * y - newHeight / 2, null);
+         */
+
         int width = image.getWidth(null) / 2;
         int height = image.getHeight(null) / 2;
-        g.drawImage(image, x - width / 2,
-                y - height / 2, null);
+        g.drawImage(image, x - width, y - height, null);
     }
 
     private boolean isClicked(Image image, double scale, int x, int y, int[] mouse) {
