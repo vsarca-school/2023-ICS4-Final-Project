0c23cbc7eac94c366c9dca3e317921d468e14f07
RadinAhari <radin.farajzadehahari@student.tdsb.on.ca>
2023-06-07 21:59:47 -0400

    d

--- src/main/Main.java
+++ src/main/Main.java
@@ -48,7 +48,7 @@ public class Main {
         e = new EndScreen();
 
         currentSceneNum = 0;
-        currentScene = o; // TODO: change back to menu when done debugging
+        currentScene = a; // TODO: change back to menu when done debugging
 
         currentScene.addToWindow(w);
 
