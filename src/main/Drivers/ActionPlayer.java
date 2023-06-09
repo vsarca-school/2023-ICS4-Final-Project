f81b4cf07cfae14ecd1fceefd86f03b5fa05d2ef
RadinAhari <radin.farajzadehahari@student.tdsb.on.ca>
2023-06-09 09:06:09 -0400

    e

--- src/main/Drivers/ActionPlayer.java
+++ src/main/Drivers/ActionPlayer.java
@@ -8,7 +8,7 @@ public class ActionPlayer extends Player {
     private int health;
     private ActionLevel parent;
 
-    public ActionPlayer(int health) {
+    public ActionPlayer(int health) { 
         this.health = health;
     }
 
