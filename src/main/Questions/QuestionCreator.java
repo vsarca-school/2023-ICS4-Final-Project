300b55742904f28f1e478dc5f14646c6c4d3496b
vsarca <vsarca@outlook.com>
2023-06-09 00:07:08 -0400

    .

---
+++ src/main/Questions/QuestionCreator.java
@@ -0,0 +1,9 @@
+package src.main.Questions;
+
+import src.main.Drivers.*;
+
+public class QuestionCreator {
+    public static void main(String[] args) {
+        Question q = new Question();
+    }
+}
