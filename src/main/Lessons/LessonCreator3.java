a6380d30b062bc2ae970e6d623d8bffa5702e798
Felix <felix.zhao2@student.tdsb.on.ca>
2023-06-08 22:40:09 -0400

    a

---
+++ src/main/Lessons/LessonCreator3.java
@@ -0,0 +1,42 @@
+package src.main.Lessons;
+
+import java.io.*;
+
+import src.main.Drivers.Lesson;
+
+/**
+ * - Victor
+ */
+public class LessonCreator3 {
+    public static void main(String argv[]) throws IOException {
+        ObjectOutputStream out;
+
+        String[] strs = {
+            "In the forest, a variety of food sources are available,\nincluding plants, fish, insects, and mushrooms.",
+            "However, it's important to exercise\ncaution and make informed choices.",
+            "While edible plants like dandelions and clovers can\nbe found throughout the forest, it's crucial to \nbe selective in your choices.",
+            "Similarly, if you encounter berries, consume them\nonly when you are absolutely certain of their safety.",
+            "When it comes to fish, lakes can provide\na viable food source.",
+            "You can fashion a makeshift net using your clothing\nor create a spear from long, sharp sticks to catch fish.",
+            "Remember to cook the fish thoroughly to eliminate\nany potential parasites or diseases.",
+            "Another often-overlooked food source\nin the forest is insects.",
+            "They are abundant and rich in nutrients.",
+            "Ants, crickets, maggots, and earthworms\nare among the edible insects you can find.",
+            "However, exercise caution and avoid consuming\nbrightly colored or hard-to-chew insects,\nas they may be dangerous.",
+            "Lastly, mushrooms can provide a valuable source\nof food, but they also come with added risk.",
+            "Pay attention to warning signs such as bright\ncolors, spots, or strangely shaped caps,\nas they may indicate poisonous varieties.",
+            "Always be meticulous and consume mushrooms only\nwhen you are completely certain about their safety.",
+            "By exercising caution and making informed choices,\nyou can successfully gather food from the forest.",
+        };
+        int[] pos = {3, 5, 6, 7, 11};
+        String[] images = {"droplet", "waterfall", "shirt", "shovel", "droplet"};
+        String next = "Lesson-4.lsn";
+        // Create level 1
+        Lesson l1 = new Lesson("Finding Water", strs, pos, "water", images, next);
+
+        // Save level 1
+        out = new ObjectOutputStream(new FileOutputStream("src/main/Lessons/Lesson-3.lsn"));
+        out.writeObject(l1);
+        out.close();
+    }
+}
